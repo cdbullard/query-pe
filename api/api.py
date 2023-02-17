22a2234e982af6e8b4b80eb50cee3a27fceedcfc
Connor Bullard <32420624+cdbullard@users.noreply.github.com>
2023-02-16 23:29:54 -0500

    CORS

--- api/api.py
+++ api/api.py
@@ -1,11 +1,13 @@
 from concurrent.futures.thread import _threads_queues
 import json
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from pglast import parser as p
 import queryParser as qp
 import phraseGenerator as pg
 
 app = Flask(__name__)
+CORS(app)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
