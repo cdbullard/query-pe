dbac13dde689a9164bc5f246fdf9460e789e7f21
Connor Bullard <32420624+cdbullard@users.noreply.github.com>
2023-02-17 00:59:41 -0500

    Update api.py

--- api/api.py
+++ api/api.py
@@ -1,25 +1,27 @@
 from concurrent.futures.thread import _threads_queues
 import json
 from flask import Flask, request, jsonify
-from flask_cors import CORS
 from pglast import parser as p
 import queryParser as qp
 import phraseGenerator as pg
 
 app = Flask(__name__)
-CORS(app)
 
 invalidSQLMessage = "Invalid."
+
+@app.after_request
+def add_cors_header(response):
+    response.headers['Access-Control-Allow-Origin'] = "https://query-pe.dev"
+    response.headers['Access-Control-Allow-Headers'] = "Content-Type,Authorization"
+    response.headers['Access-Control-Allow-Methods'] = "GET,PUT,POST,DELETE,OPTIONS"
+    return response
 
 @app.route('/parse', methods=['GET', 'POST'])
 def parseTree():
     tempVar = 0
     try:
         tempVar = 1
-        try:
-            data = json.loads(request.data)
-        except:
-            data = request.data
+        data = json.loads(request.data)
         tempVar = 2
         parseTree = p.parse_sql_json(data['inputQuery'])
         tempVar = 3
